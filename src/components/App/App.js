5f339fe267ba9b65ee8024d84cbf8590a56a2014
Amber Volkmann <amber.volkmann@brenkbrothers.com>
2021-12-10 12:16:19 -0600

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CompanyCard from '../CompanyCard/CompanyCard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <CompanyCard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
